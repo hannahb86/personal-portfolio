6fa9091cfc021bc3336b67c70ee282489e15ad47
hannahb86 <hannahbullick.design@gmail.com>
2023-02-21 12:08:00 +0000

    items completed

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import WorkItem from './WorkItem';
 
-export default function Work() {
+const data = [
+  {
+    year: '2022',
+    title: 'PF Publishing | Graphic Designer',
+    duration: 'Aug 2022 - Sep 2022',
+    details: 'Brand design for music publishing company'
+  },
+
+  {
+    year: '2021',
+    title: 'Caredial | UI/UX Designer',
+    duration: 'Feb 2021 - Jul 2021',
+    details: 'Redesign and rebranding for a care provider'
+  },
+
+  {
+    year: '2018',
+    title: 'Jorvik Media | UI/UX Designer',
+    duration: '2018 - 2019',
+    details: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid'
+  },
+
+  {
+    year: '2018',
+    title: 'Jorvik Media | UI/UX Designer',
+    duration: '2018 - 2019',
+    details: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid'
+  }
+
+]
+
+const Work = () => {
   return (
-    <div id='work'>
-      
+    <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+      <h1 className='sm:text-5xl text:4xl font-bold text-indigo-900'>Work</h1>
+      {data.map((item, idx) => (
+          <WorkItem 
+            key={idx} 
+            year={item.year} 
+            title={item.title} 
+            duration={item.duration} 
+            details={item.details} />
+        ))};
     </div>
   )
 }
+
+export default Work;
--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-export default function WorkItem() {
+const WorkItem = ({year, title, duration, details}) => {
   return (
-    <div>
-      
-    </div>
+    <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>
+      <li className='mb-10 ml-4'>
+        <div className='absolute w-3 h-3 bg-stone-300 rounded-full mt-1.5 -left-1.5 border-white'/>
+        <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
+          <span className='inline-block px-2 py-1 font-semibold text-white bg-indigo-900 rounded-md'>{year}</span>
+          <span className='text-lg font-semibold text-indigo-900'>{title}</span>
+          <span className='my-1 text-sm font-normal leading-none text-stone-500'>{duration}</span>
+        </p>
+        <p className='my-2 text-base font-normal text-stone-700'>{details}</p>
+      </li>
+    </ol>
   )
 }
+
+export default WorkItem;
