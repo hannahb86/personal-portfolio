afad424b2b4d38bb35c094b2566b750f009635c5
hannahb86 <hannahbullick.design@gmail.com>
2023-03-03 16:13:15 +0000

    Skills icons in progress

--- src/components/Skills.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Skills = () => {
-    <div id='skills' className='max-w-[1040] m-auto md:p-40 p-4 py-16'>
-        <h1 className='text-4xl font-bold text-center text-indigo-900'>Projects</h1>
-    </div>
-}
-
-export default Skills;
--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -5,8 +5,9 @@ const Tools = () => {
     <div className='max-w-[1040px] m-auto py-16'>
         <h1 className='text-4xl font-bold text-center text-indigo-900'>Skills</h1>
 
-        <div className='py-8 grid gap-1 grid-cols-5 grid-row-2'>
+        <div className='mt-16 grid gap-6 grid-cols-6 grid-row-4'>
 
+          {/* Frontend-y !! STILL TO DO - Redux */}
           <div className='min-w-120px col-start-1'>
             <img src='./src/assets/icons/HTML.png' alt='HTML icon'/>
           </div>
@@ -22,6 +23,55 @@ const Tools = () => {
           <div className='min-w-180px col-start-4'>
             <img src='./src/assets/icons/React.png' alt='React icon'/>
           </div>
+
+          <div className='min-w-180px col-start-5 row-start-1'>
+            <img src='./src/assets/icons/Tailwind.png' alt='Tailwind icon'/>
+          </div>
+
+          <div className='min-w-180px col-start-6 row-start-1'>
+            <img src='./src/assets/icons/Bootstrap.png' alt='Bootstrap icon'/>
+          </div>
+
+
+          {/* Backend-y !! STILL TO DO - Apache and Command Line */}
+          <div className='min-w-180px col-start-1 row-start-2'>
+            <img src='./src/assets/icons/JSON.png' alt='JSON icon'/>
+          </div>
+
+          <div className='min-w-180px col-start-2 row-start-2'>
+            <img src='./src/assets/icons/PHP.png' alt='PHP icon'/>
+          </div>
+
+          <div className='min-w-180px col-start-3 row-start-2'>
+            <img src='./src/assets/icons/MySQL.png' alt='MySQL icon'/>
+          </div>
+
+
+          {/* Development !! STILL TO DO - VS Code Vite CRA Next */}
+          <div className='min-w-180px col-start-1 row-start-3'>
+            <img src='./src/assets/icons/NodeJs.png' alt='Node JS icon'/>
+          </div>
+
+
+          {/* Creative */}
+          <div className='min-w-180px col-start-1 row-start-4'>
+            <img src='./src/assets/icons/Adobe.png' alt='Adobe icon'/>
+          </div>
+
+          <div className='min-w-180px col-start-2 row-start-4'>
+            <img src='./src/assets/icons/Figma.png' alt='Figma icon'/>
+          </div>
+
+          <div className='min-w-180px col-start-3 row-start-4'>
+            <img src='./src/assets/icons/Zeplin.png' alt='Zeplin icon'/>
+          </div>
+
+          <div className='min-w-180px col-start-4 row-start-4'>
+            <img src='./src/assets/icons/Unity_1.png' alt='Unity icon'/>
+          </div>
+
+          
+
         </div>
 
     </div>
