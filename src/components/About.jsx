087f45cc2f42ba8fb0f3615d76b9dc4f7f12c5fe
hannahb86 <hannahbullick.design@gmail.com>
2023-03-03 12:20:11 +0000

    about complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div id='about' className='max-w-[1040] m-auto md:pl-20 p-4 py-16'>
+    <div id='about' className='max-w-[1040] m-auto md:pl-20 p-4 py-8'>
       <h1 className='text-4xl font-bold text-center text-indigo-900'>About</h1>
-            <p className='text-center py-8 text-gray-500' >Lorem ipsum dolor sit amet 
-            consectetur, a dipisicing elit. Expedita corporis voluptas voluptate 
-            aspernatur pariatur sequi nulla perferendis omnis fuga itaque.
-            </p>
+            <p className='text-center py-4 mt-4 text-gray-500' >I have always been a creative problem solver. 
+            As a child I was the one who built the dens, the one who figured out the quickest 
+            route to the shops to buy sweets and later, the pub. </p>
+
+            <p className='text-center py-4 text-gray-500'>I love solving puzzles and have a love of computer games, particularly World of Warcraft, 
+            that I balance with yoga and kickboxing. I am also an accomplished classical guitar teacher and performer.</p>
+
+            <p className='text-center py-4 text-gray-500'>As a Front-end Developer with UI/UX qualifications, I can provide you and your business with user-centered 
+            web applications.</p>
+
+            <p className='text-center py-4 text-gray-500'>Please get in touch for more details.</p>
     </div>
   )
 }
